298da72163af2631eb0ce91a3770f6b8197d2ba2
zkdltid-chan <zkdltid.chan@gmail.com>
2024-07-04 22:07:43 +0900

    .

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,26 +1,13 @@
-import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Box, Text, Spinner } from '@chakra-ui/react';
+import React from 'react';
+import { Box, Text } from '@chakra-ui/react';
 
 const NotFound = () => {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    setTimeout(() => {
-      navigate('/'); // Redirect to the homepage or any other page
-    }, 1000); // 1 seconds delay
-  }, [navigate]);
-
   return (
     <Box textAlign="center" py={10} px={6}>
       <Text fontSize="50px">404</Text>
       <Text fontSize="24px" mt={3} mb={2}>
         Page Not Found
       </Text>
-      <Text color="gray.500" mb={6}>
-        Redirecting to the homepage...
-      </Text>
-      <Spinner size="xl" />
     </Box>
   );
 };
