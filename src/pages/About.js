4036f858a6cebce26b8d85bbf0f4da887f84446c
zkdltid-chan <zkdltid.chan@gmail.com>
2024-07-04 19:32:58 +0900

    .

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import { Box, Text } from '@chakra-ui/react';
+import { Box, Tabs, TabList, TabPanels, Tab, TabPanel, Text } from '@chakra-ui/react';
 
 const About = () => {
   return (
-    <Box>
+    <Box w="100%" maxW="container.xl" mx="auto" p={4}>
       <Text fontSize="xl" mb={4}>
         About Page
       </Text>
+      <Tabs>
+        <TabList>
+          <Tab>Tab 1</Tab>
+          <Tab>Tab 2</Tab>
+        </TabList>
+
+        <TabPanels>
+          <TabPanel>
+            <Text>Content for Tab 1</Text>
+          </TabPanel>
+          <TabPanel>
+            <Text>Content for Tab 2</Text>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
     </Box>
   );
 };
